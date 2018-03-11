a7ea3b2a51ff81ee608c64976dd709c78091d0c5
GeneralD <yumejustice@gmail.com>
2018-03-11 18:31:07 +0900

    [from now] 2018/03/11 18:31:07
    diff --git a/colorls.plugin.zsh b/colorls.plugin.zsh
    index 9e31a67..d342141 100644
    --- a/colorls.plugin.zsh
    +++ b/colorls.plugin.zsh
    @@ -1,9 +1,8 @@
     # colorls
     if which colorls 1>/dev/null; then
    -#   alias {lc,cl}='() {colorls $@ | sed "s/${HOME:gs/\//\\\//}/~/g" | sed "s/$USER/$(echo "\xef\x90\x95")/g" | sed "s/\/Dropbox/\/$(echo "\xef\x85\xab")/" | sed "s/⇒/$(echo "\xef\x83\x81")/g"}'
       alias {lc,cl}=colorls
    -  alias {lca,cla}='lc --sd -a'
    -  alias {lcl,cll}='lc --sd -l'
    +  alias {lca,cla,la}='lc --sd -a'
    +  alias {lcl,cll,ll}='lc --sd -l'
       alias l='lc --sd -al'
     else
       echo 'colorls is not installed.'

--- colorls.plugin.zsh
+++ colorls.plugin.zsh
@@ -1,9 +1,8 @@
 # colorls
 if which colorls 1>/dev/null; then
-#   alias {lc,cl}='() {colorls $@ | sed "s/${HOME:gs/\//\\\//}/~/g" | sed "s/$USER/$(echo "\xef\x90\x95")/g" | sed "s/\/Dropbox/\/$(echo "\xef\x85\xab")/" | sed "s/⇒/$(echo "\xef\x83\x81")/g"}'
   alias {lc,cl}=colorls
-  alias {lca,cla}='lc --sd -a'
-  alias {lcl,cll}='lc --sd -l'
+  alias {lca,cla,la}='lc --sd -a'
+  alias {lcl,cll,ll}='lc --sd -l'
   alias l='lc --sd -al'
 else
   echo 'colorls is not installed.'
