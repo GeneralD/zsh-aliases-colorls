6cad5c50b19d5d63b26b051c1e4ba5ebbdf86fe0
GeneralD <yumejustice@gmail.com>
2022-05-07 22:22:06 +0900

    feat: add git status --gs option

--- colorls.plugin.zsh
+++ colorls.plugin.zsh
@@ -3,7 +3,7 @@ if which colorls 1>/dev/null; then
   alias {lc,cl}=colorls
   alias {lca,cla,la}='lc --sd -a'
   alias {lcl,cll,ll}='lc --sd -l'
-  alias l='lc --sd -al'
+  alias l='lc --sd -al --gs'
 else
   echo 'colorls is not installed.'
 fi
